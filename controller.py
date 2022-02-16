badfa6b0149d4b409426165aef77c8dbe8d9ee92
marinaradomska <m.radomska00@gmail.com>
2022-02-16 21:36:05 +0100

    final V. of controller

---
+++ controller.py
@@ -0,0 +1,30 @@
+from model import *
+from view import *
+
+def turn(letter):
+    while model.getLives() > 0 and not model.checkWin():
+        res = model.turn(letter)
+        print(res, letter)
+        if res == 0:
+            return
+        if res == 1:
+            view.background(model.penalty)
+        view.drawWords(model.getVisible())
+    if model.getLives():
+        view.printSign()
+    else:
+        view.printSign(False)
+
+
+    
+def sign(x, y):
+    model.newGame()
+    view.newGame(model.getVisible())
+    
+    
+
+view = View(turn, sign)
+model = Model(7)
+view.newGame(model.getVisible())
+
+view.run()
